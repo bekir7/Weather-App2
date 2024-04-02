7b29e8e5f215e0fafdb47a0cf2b8609078ba70d9
ebubekir yıldırım <49992321+bekir7@users.noreply.github.com>
2024-04-02 20:38:05 +0300

    .
    .

--- collectapiweatherapp/lib/screens/home_page.dart
+++ collectapiweatherapp/lib/screens/home_page.dart
@@ -1,122 +1,160 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:havadurumu/models/weather_model.dart';
 import 'package:havadurumu/services/weather_service.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  List<WeatherModel> _weathers = [];
-
-  void _getWeatherData() async {
-    _weathers = await WeatherService().getWeatherData();
-
-    setState(() {});
-  }
+  late List<WeatherModel> _weathers;
+  late bool _isLoading;
 
   @override
   void initState() {
-    _getWeatherData();
     super.initState();
+    _isLoading = true;
+    _getWeatherData();
+  }
+
+  Future<void> _getWeatherData() async {
+    try {
+      final List<WeatherModel> weathers =
+          await WeatherService().getWeatherData();
+      setState(() {
+        _weathers = weathers;
+        _isLoading = false;
+      });
+    } catch (e) {
+      print('Error fetching weather data: $e');
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView.builder(
-          itemCount: _weathers.length,
-          itemBuilder: (context, index) {
-            final WeatherModel weather = _weathers[index];
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/cizim5.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 30),
+                    CarouselSlider.builder(
+                      itemCount: _weathers.length,
+                      itemBuilder: (context, index, realIndex) {
+                        final WeatherModel weather = _weathers[index];
 
-            double dereceDouble = double.parse(weather.derece);
-            int dereceInt = dereceDouble.toInt();
-            double dereceDoubleMin = double.parse(weather.min);
-            int dereceIntMin = dereceDoubleMin.toInt();
-            double dereceDoubleMax = double.parse(weather.max);
-            int dereceIntMax = dereceDoubleMax.toInt();
-            double dereceDoubleGece = double.parse(weather.gece);
-            int dereceIntGece = dereceDoubleGece.toInt();
+                        double dereceDouble = double.parse(weather.derece);
+                        int dereceInt = dereceDouble.toInt();
+                        double dereceDoubleMin = double.parse(weather.min);
+                        int dereceIntMin = dereceDoubleMin.toInt();
+                        double dereceDoubleMax = double.parse(weather.max);
+                        int dereceIntMax = dereceDoubleMax.toInt();
+                        double dereceDoubleGece = double.parse(weather.gece);
+                        int dereceIntGece = dereceDoubleGece.toInt();
 
-            return Container(
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey.shade50,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${weather.tarih}\n ${weather.gun}\n",
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black54,
-                    ),
-                  ),
-                  Column(
-                    children: [
-                      Image.network(weather.ikon, width: 100),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 15, bottom: 25),
-                        child: Text(
-                          " ${weather.durum.toUpperCase()} ${dereceInt}°",
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black54,
+                        return Container(
+                          padding: const EdgeInsets.all(10),
+                          margin:
+                              const EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.blueGrey.shade50,
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
+                          child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                "Min: ${dereceIntMin} °",
+                                "${weather.tarih}\n ${weather.gun}\n",
+                                textAlign: TextAlign.center,
                                 style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 15),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black54,
+                                ),
                               ),
-                              Text(
-                                "Max: ${dereceIntMax} °",
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 15),
+                              Image.network(weather.ikon, width: 80),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child: Text(
+                                  " ${weather.durum.toUpperCase()} ${dereceInt}°",
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black54,
+                                  ),
+                                ),
                               ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Min: ${dereceIntMin} °",
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 12),
+                                      ),
+                                      Text(
+                                        "Max: ${dereceIntMax} °",
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 12),
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        "Gece: ${dereceIntGece} °",
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 12),
+                                      ),
+                                      Text(
+                                        "Nem: ${weather.nem}%",
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 12),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              )
                             ],
                           ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                "Gece: ${dereceIntGece} °",
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 15),
-                              ),
-                              Text(
-                                "Nem: ${weather.nem}%",
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 15),
-                              ),
-                            ],
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            );
-          },
+                        );
+                      },
+                      options: CarouselOptions(
+                        aspectRatio: 5 / 3,
+                        enableInfiniteScroll: false,
+                      ),
+                    ),
+                  ],
+                ),
         ),
       ),
     );
